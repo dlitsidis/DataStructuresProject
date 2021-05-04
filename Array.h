294565fe75ea247e9b3668a669f8bca2d53f611e
Dimitris Litsidis <74056689+dlitsidis@users.noreply.github.com>
2021-05-04 16:31:35 +0300

    Update Array.h

--- Array.h
+++ Array.h
@@ -4,15 +4,16 @@ using namespace std;
 class Array
 {
   private:
-    string *words;
-    int *wordcounter;
-    int size;
+    string *words; //δείκτης που αποθηκεύει τις λέξεις που εισάγονται 
+    int *wordcounter; //δείκτης που μετράει το πλήθος τον λέξεων που έχουν εισαχθεί
+    int size; //μεταβλητή που αποθηκεύει το μεγεθος του πίνακα των λέξεων
     bool moremem(int);
     bool uniqueString(string);
+    int linearsearch(string);
   public:
-    Array();
+    Array(); //κατασκευαστής της κλάσης (αρχικοποιεί τους δείκτες σε nullptr και το μέγεθος σε 0)
 
-    void insert(string);
-    void remove(string);
-    int  search(string);
+    void insert(string); //συνάρτηση που υλοποιεί εισαγωγή στοιχείου στον πίνακα 
+    void remove(); //συνάρτηση που υλοποιεί διαγραφή στοιχείου του πίνακα
+    int  search(string); //συνάρτηση που υλοποιεί αναζήτηση στοιχείου στον πίνακα και επιστρέφει τον αριθμό εμφανίσεων   
 };
