0e1495d6b7c7c733bcc28116ca50ac1193eb7a53
Dimitris Litsidis <74056689+dlitsidis@users.noreply.github.com>
2021-05-04 16:04:56 +0300

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -2,7 +2,7 @@
 
 Array::Array()
 {
-   words=new string[245000];
+   words=nullptr;
    wordcounter=nullptr;
    size=0;
 }
@@ -11,42 +11,51 @@ void Array::insert(string a)
 {
   if(uniqueString(a))
   {
+    if(size%1000==0)
+      moremem(sizeof(string)*(size+1000));
    words[size]=a;
+   wordcounter[size]++;
    size++;
-   cout<<size<<"."<<words[size-1]<<endl;
+   //cout<<size<<"."<<words[size-1]<<endl;
   }
 }
 
-void Array::remove(string a)
+void Array::remove()
 {
-   
+   words[size-1] = "";
+   wordcounter[size-1] = 0;
+   size--;
+   moremem(size*sizeof(string));
 }
 
 int Array::search(string a)
 {
-  int i,count=0;
-  for(i=0;i<size;i++)
-  {
-    if(words[i]==a)
-    {
-      count++;
-    }
-  }
-  return count;
+  int i;
+  i=linearsearch(a);
+  return wordcounter[i];
 }
 
 bool Array::moremem(int mem)
 {
   string *temp;
+  int *temp2;
+
   temp=new string[mem];
-  if(temp==NULL)
+  temp2 = new int[mem];
+  
+  if(temp==NULL || temp2==NULL)
     return false;
+
   for(int i=0;i<size;i++)
   {
     temp[i]=words[i];
+    temp2[i] = wordcounter[i];
   }
   delete [] words;
+  delete [] wordcounter;
+
   words=temp;
+  wordcounter = temp2;
   return true;
 }
 
@@ -54,6 +63,18 @@ bool Array::uniqueString(string a)
 {
   for(int i=0;i<size;i++)
     if(a==words[i])
+    {
+      wordcounter[i]++;
       return false;
+    }
   return true;
+}
+
+int Array::linearsearch(string a)
+{
+  int i=0;
+  for(i=0;i<size;i++)
+    if(words[i]==a)
+      return i;
+    return -1;
 }
