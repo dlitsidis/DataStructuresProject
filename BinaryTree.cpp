13712a739f1781d9464c65ebbf48a180fba0d02c
Dimitris Litsidis <74056689+dlitsidis@users.noreply.github.com>
2021-05-29 16:04:43 +0300

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -10,7 +10,10 @@ BinaryTree::BinaryTree()
 void BinaryTree::insert(string a)
 {
   if(root==nullptr)
+  {
     root=new node(a);
+    return ;
+  }
   
   insertNode(root,a);
 }
@@ -43,7 +46,7 @@ bool BinaryTree::insertNode(node* root,string value)
     return true;
 }
     
-struct node* search(struct node* root, string key)
+node* BinaryTree::search(node* root, string key)
 {
     if (root == nullptr || root->data.value == key)
        return root;
@@ -56,6 +59,17 @@ struct node* search(struct node* root, string key)
     {
       return search(root->leftchild, key);
     }
+}
+
+bool BinaryTree::search(string key)
+{
+  node *n=new node();
+  n=search(root,key);
+  if(n==nullptr)
+   return false;
+  int c=n->data.counter;
+  cout<<"\""<<n->data.value<<"\""<<" was found: "<<c<<" times"<<endl;
+  return true;
 }
 
 void BinaryTree::printPostOrder()
@@ -104,8 +118,67 @@ void BinaryTree::printInOrder(node * r)
 
   printInOrder(r->leftchild);
 
-  cout <<r->data.value <<endl;
+  cout <<r->data.value <<": "<<r->data.counter <<endl;
  
   printInOrder(r->rightchild);
+}
+
+node* BinaryTree::min(node *r)
+{
+  if(r==nullptr)
+    return nullptr;
+  node *min;
+  min=r;
+  while(min->leftchild!=nullptr)
+    min=min->leftchild;
+  return min;
+}
+
+node* BinaryTree::remove(node *root,string a)
+{
+    if (root==nullptr)
+        return root;
+ 
+    if (a< root->data.value)
+        root->leftchild = remove(root->leftchild, a);
+    else if (a > root->data.value)
+        root->rightchild = remove(root->rightchild, a);
+    else 
+    {
+        if (root->leftchild==nullptr and root->rightchild==nullptr)
+        {
+            //free(root);
+            return nullptr;
+        }
+        else if (root->leftchild==nullptr)
+        {
+            node* temp=root->rightchild;
+            free(root);
+            return temp;
+        }
+        else if (root->rightchild==nullptr) {
+            node* temp = root->leftchild;
+            free(root);
+            return temp;
+        }
+ 
+        node* temp = min(root->rightchild);
+        root->data.value = temp->data.value;
+        root->rightchild= remove(root->rightchild, temp->data.value);
+    }
+    return root;
+}
+
+bool BinaryTree::remove(string a)
+{
+  if(search(root,a)!=nullptr)
+  {
+    remove(root,a);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
